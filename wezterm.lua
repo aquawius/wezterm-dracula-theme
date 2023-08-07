4672f7f9b4122223c55c7a09160bf17408d5c609
aquawius <aquawius@outlook.com>
2023-08-08 00:09:58 +0800

    wezterm config: Add some pwsh(powershell7) Prompt for windows.

--- wezterm.lua
+++ wezterm.lua
@@ -1,14 +1,12 @@
 -- Wezterm configuration
 -- powered by aquawius
 -- this is version 4
-
 -- version 1: initial config
 -- version 2: wsl support
 -- version 3: update theme to purple style
--- version 4: fix bug "git log" with "terminal is not fully functional" 
+-- version 4: fix bug "git log" with "terminal is not fully functional"
 --            tracert: term set to "" is not a compatible term for git
 -- version 5: update theme to dracula official
-
 local wezterm = require("wezterm");
 local dracula = require('dracula');
 
@@ -31,22 +29,16 @@ local config = {
     inactive_pane_hsb = {
         hue = 1.0,
         saturation = 1.0,
-        brightness = 1.0,
+        brightness = 1.0
     },
 
     -- font = wezterm.font(''),
-    font = wezterm.font_with_fallback({
-        "CaskaydiaCove Nerd Font",      -- add Cascadia Code Nerd Font
-        "Cascadia Code",
-        "Fira Code",
-    }),
-    font_size = 15.0,
+    font = wezterm.font_with_fallback({"CaskaydiaCove Nerd Font", "Cascadia Code", "Fira Code"}),
+    font_size = 14.0,
 
-    default_prog = { 'pwsh' },
+    default_prog = {'pwsh'},
     -- default_cwd = "/some/path",
-    launch_menu = {
-
-    },
+    launch_menu = {},
 
     -- leader = { key = "b", mods = "CTRL" },
     set_environment_variables = {},
@@ -98,25 +90,54 @@ local config = {
     use_fancy_tab_bar = false
 }
 
-
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
     -- config.term = "" -- Set to empty so FZF works on windows
     -- config.term = "xterm"  -- fix bug in command "git log" with "terminal is not fully functional" or delete this term = "xxxx" (using default term value)
 
-    table.insert(config.launch_menu, { label = "PowerShell 5", args = { "powershell.exe", "-NoLogo" } })
-    table.insert(config.launch_menu, { label = "PowerShell 7", args = { "pwsh.exe", "-NoLogo" } })
-    table.insert(config.launch_menu,
-        { label = "VS PowerShell 2022", args = { "powershell", "-NoLogo", "-NoExit", "-Command", "devps 17.0" } })
-    table.insert(config.launch_menu,
-        { label = "VS PowerShell 2019", args = { "powershell", "-NoLogo", "-NoExit", "-Command", "devps 16.0" } })
-    table.insert(config.launch_menu, { label = "Command Prompt", args = { "cmd.exe" } })
-    table.insert(config.launch_menu,
-        { label = "VS Command Prompt 2022", args = { "powershell", "-NoLogo", "-NoExit", "-Command", "devcmd 17.0" } })
-    table.insert(config.launch_menu,
-        { label = "VS Command Prompt 2019", args = { "powershell", "-NoLogo", "-NoExit", "-Command", "devcmd 16.0" } })
+    table.insert(config.launch_menu, {
+        label = "Command Prompt",
+        args = {"cmd.exe"}
+    })
+    table.insert(config.launch_menu, {
+        label = "PowerShell 5",
+        args = {"powershell.exe", "-NoLogo"}
+    })
+    table.insert(config.launch_menu, {
+        label = "PowerShell 7",
+        args = {"pwsh.exe", "-NoLogo"}
+    })
+
+    table.insert(config.launch_menu, {
+        label = "Anaconda PowerShell Prompt",
+        args = {"pwsh", "-NoLogo", "-NoExit", "-ExecutionPolicy", "Bypass", "-Command",
+                "& 'C:\\ProgramData\\anaconda3\\shell\\condabin\\conda-hook.ps1' ; conda activate 'C:\\ProgramData\\anaconda3' "}
+    })
+
+    table.insert(config.launch_menu, {
+        label = "VS Command Prompt 2022 (PowerShell 7)",
+        args = {"pwsh", "-NoLogo", "-NoExit", "-ExecutionPolicy", "Bypass", "-NoProfile", "-Command",
+                " & 'C:\\Program Files\\Microsoft Visual Studio\\2022\\Professional\\Common7\\Tools\\Launch-VsDevShell.ps1'"}
+    })
+
+    table.insert(config.launch_menu, {
+        label = "VS PowerShell 2022",
+        args = {"powershell", "-NoLogo", "-NoExit", "-Command", "devps 17.0"}
+    })
+    table.insert(config.launch_menu, {
+        label = "VS PowerShell 2019",
+        args = {"powershell", "-NoLogo", "-NoExit", "-Command", "devps 16.0"}
+    })
+    table.insert(config.launch_menu, {
+        label = "VS Command Prompt 2022",
+        args = {"powershell", "-NoLogo", "-NoExit", "-Command", "devcmd 17.0"}
+    })
+    table.insert(config.launch_menu, {
+        label = "VS Command Prompt 2019",
+        args = {"powershell", "-NoLogo", "-NoExit", "-Command", "devcmd 16.0"}
+    })
 
     -- Enumerate any WSL distributions that are installed and add those to the menu
-    local success, wsl_list, wsl_err = wezterm.run_child_process({ "wsl", "-l" })
+    local success, wsl_list, wsl_err = wezterm.run_child_process({"wsl", "-l"})
     -- `wsl.exe -l` has a bug where it always outputs utf16:
     -- https://github.com/microsoft/WSL/issues/4607
     -- So we get to convert it
@@ -132,20 +153,23 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
             -- Add an entry that will spawn into the distro with the default shell
             table.insert(config.launch_menu, {
                 label = distro .. " (WSL default shell)",
-                args = { "wsl", "--distribution", distro },
+                args = {"wsl", "--distribution", distro}
             })
 
             -- Here's how to jump directly into some other program; in this example
             -- its a shell that probably isn't the default, but it could also be
             -- any other program that you want to run in that environment
-            table.insert(config.launch_menu, {
-                label = distro .. " (WSL zsh login shell)",
-                args = { "wsl", "--distribution", distro, "--exec", "/bin/zsh", "-l" },
-            })
+            -- table.insert(config.launch_menu, {
+            --     label = distro .. " (WSL zsh login shell)",
+            --     args = { "wsl", "--distribution", distro, "--exec", "/bin/zsh", "-l" },
+            -- })
         end
     end
 else
-    table.insert(config.launch_menu, { label = "zsh", args = { "zsh", "-l" } })
+    table.insert(config.launch_menu, {
+        label = "zsh",
+        args = {"zsh", "-l"}
+    })
 end
 
 -- Equivalent to POSIX basename(3)
@@ -158,9 +182,9 @@ end
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
     local pane = tab.active_pane
     local title = Basename(pane.foreground_process_name)
-    return {
-        { Text = " " .. title .. " " },
-    }
+    return {{
+        Text = " " .. title .. " "
+    }}
 end)
 
 return config
